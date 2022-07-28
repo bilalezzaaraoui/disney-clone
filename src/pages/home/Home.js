7d257c3077640e2b612f0bb06b8d5e8c84e3dfc0
bilal <bilalezzaa1@gmail.com>
2022-07-28 12:38:17 +0200

    Ajout du carrousel

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,31 @@
 import styled from "styled-components";
+import HomeBg from "../../asset/images/home-background.png";
+import ImgSlider from "../../components/ui/ImgSlider";
 
 const Home = (props) => {
-  return <div>Homepage</div>;
+  return (
+    <Container>
+      <ImgSlider />
+    </Container>
+  );
 };
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh- 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url(${HomeBg}) center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default Home;
